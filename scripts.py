c06a3f7f6eaca5a080fb0f88033aab7aac322112
Jithuze <125732084+Jithuze@users.noreply.github.com>
2024-11-13 22:13:06 +0530

    Delete scripts.py

--- scripts.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-try:
-    subprocess.run("anydesk", shell=True, check=True)
-    subprocess.run("playit", shell=True, check=True)
-except subprocess.CalledProcessError as e:
-    print(f"Error executing Startup command: {e}")
